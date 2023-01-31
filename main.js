60b46c198e218454746309bb30a597d6165fcdc5
git.assignment-1 <cesskui4034@gmail.com>
2023-01-31 16:09:26 +0300

    cess

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addeventlistener ('load',() => {
+    const form =document.querySelector("#new-task-form");
+    const input =document.querySelector("new-task-input");
+    const list_el =document.querySelector( "#tasks");
+
+    console.log(form);
+})
